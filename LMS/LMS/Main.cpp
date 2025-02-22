3ecb9d6c5a15baee1926c3bb5563cabd593bdaa2
Roshan Kewat <roshankewat9090@gmail.com>
2025-02-22 17:54:18 +0530

    Add Book and List Book Completed

--- LMS/LMS/Main.cpp
+++ LMS/LMS/Main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include<string>
 
 
 class Library {
@@ -21,6 +22,10 @@ public:
 	}
 
 	void ShowAllBooks() {
+		if (books.empty()) {
+			std::cout << "No Books Available Right Now" << std::endl;
+			return;
+		}
 		std::cout << "All Available Books" << std::endl;
 		for (int i = 0;i < books.size();i++) {
 			std::cout << books.at(i) << std::endl;
@@ -30,7 +35,39 @@ public:
 
 int main() {
 	Library* library = new Library("RK Library");
-	std::string userCommand = "";
-	std::cin >> userCommand;
+
+	std::cout << "Commands" << std::endl;
+	std::cout << "0-> ShowAllBooks" << std::endl;
+	std::cout << "1-> AddBook" << std::endl;
+	std::cout << "2-> DeleteBook" << std::endl;
+	std::cout << "3-> BorrowBook" << std::endl;
+	std::cout << "4-> ReturnBook" << std::endl;
+	std::cout << "5-> Exit" << std::endl;
+
+	int userCommand = 0;
+	std::string bookName = "";
+	while (userCommand != 5) {
+		if (userCommand > 5)
+			std::cout << "Invaild Command"<<std::endl;
+
+		std::cout << "Command>> ";
+		std::cin >> userCommand;
+		std::cin.ignore();
+
+		switch (userCommand)
+		{
+            case 0:
+				library->ShowAllBooks();
+				break;
+			case 1:
+				std::cout << "Book Name-> ";
+				std::getline(std::cin, bookName);
+				library->AddBooks(bookName);
+				break;
+		default:
+			break;
+		}
+	}
+	delete library;
 	return 0;
 }
